edee809079ac7447222907fcd324ea2797e260c9
Aidan Quinn <quinn0219@gmail.com>
2018-12-05 19:51:29 -0500

    continuing

--- AQ_RNAseq.sh
+++ AQ_RNAseq.sh
@@ -4,9 +4,7 @@
 # Functions
 welcome() {
     clear
-    echo "
-
-#################################################################################################################
+    echo "#################################################################################################################
 #                                                                                                               #
 #                                    Welome to AQ_RNAseq pipeline!                                              #
 #                                      · v.0.1.0 · 2018.12.05 ·                                                 #
@@ -21,8 +19,8 @@ Usage: AQ_RNAseq.sh [options]
                                                                                                                
      Required Arguments:                                                                                       
         -g | --genome_index            Can be one of [ GRCh38 or mm10 ]. The full path to the STAR index       
-                                       must be specified in AQ_RNAseq_settings.txt as genome_index_dir.        
-                                                                                                               
+                                       must be specified in AQ_RNAseq_settings.txt as genome_index_dir.
+
         -s | --samples_file            Sample file should be formated as tsv txt file.
         
         -p | --parameters_file         File containing AQ_RNAseq run basic paramerts. Tab separated key 
@@ -39,6 +37,9 @@ Usage: AQ_RNAseq.sh [options]
     
         -t | --threads_max             Maximum number of threads to use for all steps.
 
+        -f | --force_overwrite         Danger! This option will allow the pipeline to overwrite files but
+                                       will ensures a clean output. Default: FALSE [ TRUE or FALSE]
+
         -h | --help                    Prints this message.
 
 ##################################################################################################################
@@ -47,6 +48,10 @@ Usage: AQ_RNAseq.sh [options]
 
 # Print weclome message
 welcome
+
+# Set defaults
+threads_max=1
+force_overwrite="FALSE"
 
 # Collect options or print usage statement
 POSITIONAL=()
@@ -83,6 +88,11 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        -f | --force_overwrite )
+            force_overwrite="$2"
+            shift
+            shift
+            ;;
         -v | --verbose )
             verbose="TRUE"
             shift
@@ -93,6 +103,16 @@ while [[ $# -gt 0 ]]; do
             exit 1
     esac
 done
+
+# Warn about overwrite
+if [ "$force_overwrite" == "TRUE" ]; then
+    printf "[WARNING] Set to overwrite files! Ctl + c now if you didn't mean to overwrite..."
+    for i in {5..1}; do
+        printf '%s...' "$i"
+        sleep 1
+    done
+    printf "\n"
+fi
 
 ## Debugging
 #printf "Params:\n 
@@ -119,30 +139,58 @@ if [ ! -f "${samples_file}" ]; then
 fi
 
 # Read the columns of samples_file as separate arrays
-while read col1 col2 ; do 
+while read col1 col2 col3 col4; do 
     samples+=($col1)
-    read_len+=($col2)
+    read_lens+=($col2)
+    fastq_paths_1+=($col3)
+    fastq_paths_2+=($col4)
 done < <(sed 's/;/\t/g' $samples_file)
 
 # Check that we have a read len for every sample
-if [ ! "${#samples[@]}" = "${#read_len[@]}" ]; then
+if [ ! "${#samples[@]}" -eq "${#read_lens[@]}" ]; then
     echo "[ERROR] Check your samples file. number of sample != number of read length.\n"
     exit 1
 fi
 
-## Debugging
+# Debugging
 printf 'Found %s samples: \n' "${#samples[@]}"
 printf '%s\n' "${samples[@]}"
 echo ""
 printf 'Read lengths are:\n'
-printf '%s\n' "${read_len[@]}"
+printf '%s\n' "${read_lens[@]}"
+
+# Get status of status of mate-pairs
+for i in "${!samples[@]}"; do
+    if [ -f "${fastq_paths_2[${i}]}" ]; then
+        is_paireds+=("TRUE")
+    else
+        is_paireds+=("FALSE")
+    fi 
+done
+
+# Debugging
+printf 'Paired statuses are:\n'
+printf '%s\n' "${is_paireds[@]}"
+
+# Validate fastq files
+for i in "${!samples[@]}"; do
+    if [ ! -f "$fastq_paths_1[$i]" ]; then
+        printf '[ERROR] Could not find fastq files for %s \n' "${samples[$i]}"
+        exit 1
+    fi
+done
+
+
 ##################################################################################################################
 # ALIGNMENT
 ##################################################################################################################
 
-# Dynamic read_len support
+# NOTES:
+# · Add dynamic read_len support
+# · Check that STAR is in $PATH
 
-# Collect and validate required params
+# Collect and validate additional required params
+# star index:
 star_index_path="${genome_index_dir}/${genome_index}_${read_len}"
 if [ ! -d "${star_index_path}" ]; then
     printf "[ERROR] Could not find genome index. Looked here: ${star_index_path} \n\n"
@@ -150,10 +198,26 @@ if [ ! -d "${star_index_path}" ]; then
 fi
 
 
+# Make aligned directory in current working dir
+if [ "${force_overwrite}" == "TRUE" ]; then
+    rm -rf ./aligned; mkdir ./aligned
+else
+    mkdir -p ./aligned
+fi
 
 
-
-
+# Begin Alignment
+printf 'Beginning STAR alignemnt on %s threads...\n' "${threads_max}"
+for i in "${!samples[@]}"; do
+    printf 'Processing %s' "${samples[$i]}"
+    STAR \
+        --runThreadN "${threads_max}" \
+        --genomeDir "${star_index_path}" \
+        --readFilesIn "${fastq_paths_1[$i]}" "${fastq_paths_2[$i]}" \
+        --readFilesCommand zcat \
+        --outFileNamePrefix "./aligned/${samples[$i]}_${genome_index}_" \
+        --outSAMtype BAM Unsorted
+done
 
 ## Debugging
 #echo "--genome_index = ${genome_index}"
